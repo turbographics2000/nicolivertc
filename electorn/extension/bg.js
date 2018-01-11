9e060ae7c25cd10b157cf6de2852677613104693
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-01-11 18:45:11 +0900

    commit

--- electorn/extension/bg.js
+++ electorn/extension/bg.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((msg, sender, res) => {
     if (msg.request) {
-        chrome.desktopCapture.chooseDesktopMedia([msg.type], streamId => {
+        chrome.desktopCapture.chooseDesktopMedia([msg.type], sender.tab, streamId => {
             chrome.tabs.sendMessage(sender.tab.id, { streamId, type: msg.type });
         });
     }
